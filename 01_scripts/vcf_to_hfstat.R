f0828ca0585bf1800c5278ad4f44afe5c8a9ac54
bensutherland <bensutherland7@gmail.com>
2018-09-21 15:50:27 -0700

    Add Rscript for analyzing vcf in hierfstat

--- 01_scripts/vcf_to_hfstat.R
+++ 01_scripts/vcf_to_hfstat.R
@@ -1,5 +1,5 @@
 # VCF to hierfstat
-setwd("/Users/wayne/Documents/my_space/05_school-awards-talks/03_conferences_and_talks/2018-SING_genomics_SFU")
+setwd("~/Documents/other_research_associates/breden_felix_2018/SING_Canada_2018_genetics")
 
 # source("https://bioconductor.org/biocLite.R")
 # biocLite("VariantAnnotation")
@@ -15,49 +15,34 @@ library("hierfstat")
 # install.packages("ape")
 library("ape")
 
-vcf <- read.vcfR("butter_clam_out.recode.vcf")
+#### 1. Import and set up genlight ####
+# Import the recoded vcf
+vcf <- read.vcfR("03_vcf/aln_maf.recode.vcf")
 vcf
 
-x <- vcfR2genlight(vcf)
-x
-indNames(x)
+# Make genlight file (but note only loci with two alleles will be kept, not haplotypes. 1 locus is dropped)
+my.data.gl <- vcfR2genlight(vcf)
+my.data.gl
 
+indNames(my.data.gl) # provides the individual names
+pop(my.data.gl)  # provides the pop names (currently NULL)
 
-pop(x) 
-# currently the population is null, replace with the ind name component
-pops <- gsub(pattern = ".*\\|", replacement = "", x =  indNames(x))
+# replace the pop with the relevant section of the individual names
+pops <- gsub(pattern = ".*\\|", replacement = "", x =  indNames(my.data.gl))
+# remove subsite within each location
 pops <- gsub(pattern = "\\_.*", replacement = "", x =  pops) 
-pop(x) <- pops
+# replace null w/ new pops variable
+pop(my.data.gl) <- pops
+pop(my.data.gl)
 
-pop(x)
+# Drop the individual with an 'incomplete' designation
+my.data.gl <- my.data.gl[pop(my.data.gl) != "INCOMPLETE"] # remove the incomplete record
+pop(my.data.gl)
+my.data.gl
 
-# x <- x[indNames(x) != "BUTTERCLAM_01|RUSSELL"]
-# x <- x[indNames(x) != "BUTTERCLAM_02|RUSSELL"]
-# x <- x[indNames(x) != "BUTTERCLAM_31|PORTLAND_SHELLBE"]
-
-x
-
-# Run PCA
-pca1 <- glPca(x, nf = 3)
-scatter(x = pca1, posi = "bottomright", xax = 1, yax = 2)
-title("PC1 (x) vs PC2 (y)", adj = 1)
-
-# Run dapc
-dapc1 <- dapc(x, n.pca = 10, n.da = 1)
-scatter(dapc1, scree.da = F, bg = "white", legend = T
-        , txt.leg=rownames(dapc1$means)
-        , posi.leg = "topleft"
-)
-
-compoplot(dapc1
-          #, lab="" # comment out if you want sample labels
-          , txt.leg = rownames(dapc1$means)
-          , posi = "topright"
-          #          , cex = 0.7
-          , cex.names = 0.6 
-)
-
-myFreq <- glMean(x)
+#### 2. Run data exploration/multidimensional scaling stats ####
+# MAF plot
+myFreq <- glMean(my.data.gl)
 hist(myFreq, proba=T, col="gold", xlab = "Allele frequencies"
      , main = "Distribution of (second) allele frequencies"
      , ylim = c(0,50)
@@ -66,8 +51,31 @@ text(x = 0.4, y = 7, labels = paste(nLoc(my.data), " loci", sep = "" ))
 temp <- density(myFreq)
 lines(temp$x, temp$y, lwd=3)
 
+## Multidimensional scaling stats are not really working, probably because so few loci
+# pca1 <- glPca(my.data.gl, nf = 2)
+# scatter(x = pca1, posi = "bottomright", xax = 1, yax = 2)
+# title("PC1 (x) vs PC2 (y)", adj = 1)
 
-y <- seploc(x, n.block=10, parallel=F)
+# Run dapc
+## Also not really working, due to so few loci
+# dapc1 <- dapc(my.data.gl, n.pca = 10, n.da = 1)
+# scatter(dapc1, scree.da = F, bg = "white", legend = T
+#         , txt.leg=rownames(dapc1$means)
+#         , posi.leg = "topleft"
+# )
+
+# Compoplot
+# compoplot(dapc1
+#           #, lab="" # comment out if you want sample labels
+#           , txt.leg = rownames(dapc1$means)
+#           , posi = "topright"
+#           #          , cex = 0.7
+#           , cex.names = 0.6 
+# )
+
+#### 3. Neighbour joining tree ####
+# Separate loci
+y <- seploc(my.data.gl, n.block=10, parallel=F)
 
 ## Use dist in a lapply loop to compute pairwise distances between individuals for each block
 lD <- lapply(y, function(e) dist(as.matrix(e)))
@@ -79,34 +87,36 @@ class(lD[[1]]) # lD is a list of distance matrices bw pairs of indiv
 D <- Reduce("+", lD)
 
 # Plot a neighbor-joining tree using ape's nj function on the distance matrix
-par(mar=c(4,4,4,4))
-plot(nj(D), type="fan", cex=0.7)
+pdf(file = "04_results/njt.pdf", width = 7, height = 5)
+par(mar=c(4,3,5,3))
+plot(nj(D), type="fan", cex=0.3)
+dev.off()
 
-
+#### 4. Set up for hierfstat ####
 # Convert to genind format
-my.data <- x
-
-my.data.mat <- as.matrix(my.data)
-my.data.mat[1:5,1:5]
+# First by converting to matrix
+my.data.mat <- as.matrix(my.data.gl)
+my.data.mat[1:8,]
 
 # Second, translate the number of minor allele to genind format
 my.data.mat[my.data.mat == 0] <- "1/1" #homozygote reference
-my.data.mat[my.data.mat == 0] <- "1/2" #heterozygote
-my.data.mat[my.data.mat == 1] <- "2/2" #homozygote alternate
+# my.data.mat[my.data.mat == 0] <- "1/2" #heterozygote are not possible here
+my.data.mat[my.data.mat == 2] <- "2/2" #homozygote alternate
 #my.data.mat[my.data.mat == "NA"] <- "NA" # NA
-my.data.mat[1:5,1:5]
+my.data.mat[1:8,]
 
 # Convert matrix to genind
-my.data.gid <- df2genind(my.data.mat, sep = "/", ploidy = 2) # convert df to genind
+my.data.gid <- df2genind(my.data.mat, sep = "/", ploidy = 2) # convert df to genind (NOTE: ploidy = 2 here, but actually = 1)
 
-# Generate populations for genind
+# Generate population IDs for genind file
 pop(my.data.gid) # currently null
 indNames(my.data.gid) # The individual names actually contain pop IDs
-pops <- gsub(pattern = ".*\\|", replacement = "", x =  indNames(x)) 
+pops <- gsub(pattern = ".*\\|", replacement = "", x =  indNames(my.data.gid)) 
 pop(my.data.gid) <- gsub(pattern = "\\_.*", replacement = "", x =  pops) 
 pop(my.data.gid)
 
-# Show sample size per population
+# Barplot sample size per population
+pdf(file = "04_results/sample_size_per_pop.pdf", width = 7, height = 5)
 par(mar=c(7,4,4,4))
 table(pop(my.data.gid))
 barplot(table(pop(my.data.gid)), col=funky(17)
@@ -116,7 +126,7 @@ barplot(table(pop(my.data.gid)), col=funky(17)
         , ylab="Sample size"
         , ylim = c(0,25))
 abline(h = c(10,20,30), lty=2)
-
+dev.off()
 
 # How many alleles? (should all be 2)
 table(nAll(my.data.gid))
@@ -125,19 +135,32 @@ table(nAll(my.data.gid))
 all.data.hf <- genind2hierfstat(my.data.gid)
 rownames(all.data.hf) <- indNames(my.data.gid)
 
-# Remove the LITTLENECKCLAM from RUSSELL POP
-all.data.hf <- all.data.hf[-64,]
-rownames(all.data.hf)
+# # Remove the LITTLENECKCLAM from RUSSELL POP
+# all.data.hf <- all.data.hf[-64,]
+# rownames(all.data.hf)
 
 # Pairwise Fst
 pairwise.wc.fst <- pairwise.WCfst(all.data.hf)
 pairwise.wc.fst
-#write.csv(pairwise.wc.fst, file = "11-other_stats/all_data_wcfst.csv")
+write.csv(pairwise.wc.fst, file = "04_results/all_data_wcfst.csv")
 
 # Bootstrapping
 # requires latest hierfstat (v0.04-29) otherwise get error
 # library(devtools)
 # install_github("jgx65/hierfstat")
 # library("hierfstat")
-boot.fst.all <- boot.ppfst(dat = all.data.hf, nboot = 100000, quant = c(0.025,0.975))
+options(scipen = 999999)
+nboots <- 100000
+boot.fst.all <- boot.ppfst(dat = all.data.hf, nboot = nboots, quant = c(0.025,0.975))
 boot.fst.all
+
+# Collect output
+lower.limit <- t(boot.fst.all$ll)
+upper.limit <- boot.fst.all$ul
+upper.limit[is.na(upper.limit)] <- 0
+lower.limit[is.na(lower.limit)] <- 0
+boot.fst.all.output <- upper.limit + lower.limit
+boot.fst.all.output
+
+filename <- paste0("04_results/fst_nboot_", nboots, ".csv")
+write.csv(x = boot.fst.all.output, file = filename)
